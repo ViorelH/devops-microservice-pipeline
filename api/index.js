920842514a83b5f3fdb23bb35eeb2f4e249c1b0a
Viorel Hirjoghi <viorel.hirjoghi@gmail.com>
2025-06-23 23:55:39 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.get('/api', (req, res) => res.send('Hello from API!'));
+
+app.listen(port, () => console.log(`API running on port ${port}`));
